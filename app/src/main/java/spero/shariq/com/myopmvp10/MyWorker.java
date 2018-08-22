08d24bf4287db0ad6de13ad1b3db67beee81ce50
drshariqalikhan <speromedtech@gmail.com>
2018-08-22 22:03:50 +0800

    second

--- app/src/main/java/spero/shariq/com/myopmvp10/MyWorker.java
+++ app/src/main/java/spero/shariq/com/myopmvp10/MyWorker.java
@@ -1,14 +1,27 @@
 package spero.shariq.com.myopmvp10;
 
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
 
+import java.util.Calendar;
 import java.util.List;
 
 import androidx.work.Worker;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 /**
  * Created by drsha on 18/8/2018.
@@ -19,6 +32,7 @@ public class MyWorker extends Worker{
     ApiInterface apiInterface;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
+    JourneyTime journeyTime;
 
     @NonNull
     @Override
@@ -26,6 +40,7 @@ public class MyWorker extends Worker{
         sharedPreferences = getApplicationContext().getSharedPreferences("mypref", 0);
         editor = sharedPreferences.edit();
         apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+        journeyTime = new JourneyTime(getApplicationContext());
 
         Call<List<JourneyData>> call =  apiInterface.getData("TOKEN "+sharedPreferences.getString("token","xxx"));
         call.enqueue(new Callback<List<JourneyData>>() {
@@ -33,6 +48,7 @@ public class MyWorker extends Worker{
             public void onResponse(Call<List<JourneyData>> call, Response<List<JourneyData>> response) {
                 if(response.isSuccessful()){
                    SyncDataWithServer(response,editor);
+                   ShowNotificationforJp();
 
                 }else
                 {
@@ -49,7 +65,7 @@ public class MyWorker extends Worker{
 
         return Result.RETRY;
     }
-    void SyncDataWithServer(Response<List<JourneyData>> response, SharedPreferences.Editor editor)
+    private void SyncDataWithServer(Response<List<JourneyData>> response, SharedPreferences.Editor editor)
     {
         //TODO:Syncs Vars in Shared Prefernces using SyncDataWithServer()
         //vars :
@@ -94,4 +110,109 @@ public class MyWorker extends Worker{
 
     }
 
+    void ShowNotificationforJp() {
+        //start showing notif at start and  stop at deadline
+
+        switch (sharedPreferences.getString("journey_point", "xxx")) {
+
+            case "preop_incomplete":
+                //if current time is between than stage start & deadline : set alert dialogue and set show notification to TRUE
+                if (Calendar.getInstance().getTime().after(journeyTime.PREOP_START) && Calendar.getInstance().getTime().before(journeyTime.PREOP_DEADLINE)) {
+                    showNotification();
+                }
+
+            case "preop_GotoClinic":
+                //if current time is between than stage start & deadline : set alert dialogue and set show notification to TRUE
+                if (Calendar.getInstance().getTime().after(journeyTime.PREOP_START) && Calendar.getInstance().getTime().before(journeyTime.PREOP_DEADLINE)) {
+                    showNotification();
+                }
+
+
+            case "preop_drug_incomplete":
+                //if current time is between than stage start & deadline : set alert dialogue and set show notification to TRUE
+                if (Calendar.getInstance().getTime().after(journeyTime.PREOP_START) && Calendar.getInstance().getTime().before(journeyTime.PREOP_DEADLINE)) {
+                    showNotification();
+                }
+
+
+            case "reminder1_incomplete":
+                //if current time is between than stage start & deadline : set alert dialogue and set show notification to TRUE
+                if (Calendar.getInstance().getTime().after(journeyTime.REMINDER1_START) && Calendar.getInstance().getTime().before(journeyTime.REMINDER1_DEADLINE)) {
+                    showNotification();
+                }
+
+
+            case "reminder2_incomplete":
+                //if current time is between than stage start & deadline : set alert dialogue and set show notification to TRUE
+                if (Calendar.getInstance().getTime().after(journeyTime.REMINDER2_START) && Calendar.getInstance().getTime().before(journeyTime.REMINDER2_DEADLINE)) {
+                    showNotification();
+                }
+
+
+
+            case "pod1_incomplete":
+
+                //if current time is between than stage start & deadline : set alert dialogue and set show notification to TRUE
+                if (Calendar.getInstance().getTime().after(journeyTime.POST_OP1_START) && Calendar.getInstance().getTime().before(journeyTime.POST_OP1_DEADLINE)) {
+                    showNotification();
+                }
+
+
+            case "pod2_incomplete":
+                //if current time is between than stage start & deadline : set alert dialogue and set show notification to TRUE
+                if (Calendar.getInstance().getTime().after(journeyTime.POST_OP2_START) && Calendar.getInstance().getTime().before(journeyTime.POST_OP2_DEADLINE)) {
+                    showNotification();
+                }
+
+
+            case "pod3_incomplete":
+                //if current time is between than stage start & deadline : set alert dialogue and set show notification to TRUE
+                if (Calendar.getInstance().getTime().after(journeyTime.POST_OP3_START) && Calendar.getInstance().getTime().before(journeyTime.POST_OP3_DEADLINE)) {
+                    showNotification();
+                }
+
+
+
+            case "pod5_incomplete":
+                //if current time is between than stage start & deadline : set alert dialogue and set show notification to TRUE
+                if (Calendar.getInstance().getTime().after(journeyTime.POST_OP5_START) && Calendar.getInstance().getTime().before(journeyTime.POST_OP5_DEADLINE)) {
+                    showNotification();
+                }
+
+
+
+            case "pod10_incomplete":
+                //if current time is between than stage start & deadline : set alert dialogue and set show notification to TRUE
+                if (Calendar.getInstance().getTime().after(journeyTime.POST_OP10_START) && Calendar.getInstance().getTime().before(journeyTime.POST_OP10_DEADLINE)) {
+                    showNotification();
+                }
+
+
+            case "pod15_incomplete":
+                //if current time is between than stage start & deadline : set alert dialogue and set show notification to TRUE
+                if (Calendar.getInstance().getTime().after(journeyTime.POST_OP15_START) && Calendar.getInstance().getTime().before(journeyTime.POST_OP15_DEADLINE)) {
+                    showNotification();
+                }
+
+
+
+        }
+    }
+    private void showNotification()
+    {
+        PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 0, new Intent(getApplicationContext(), TiledDashboard.class), 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(),"personal");
+        builder.setSmallIcon(android.R.drawable.ic_menu_report_image);
+        builder.setContentTitle("IMPORTANT TASK FROM MYOP!");
+        builder.setContentText("Please click here complete stage now!");
+        builder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL);
+        builder.setContentIntent(pi);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
+        notificationManagerCompat.notify(001,builder.build());
+
+    }
+
 }
+
--- app/src/main/java/spero/shariq/com/myopmvp10/pod15.java
+++ app/src/main/java/spero/shariq/com/myopmvp10/pod15.java
@@ -1,13 +1,72 @@
 package spero.shariq.com.myopmvp10;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class pod15 extends AppCompatActivity {
-
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    ApiInterface apiInterface;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pod15);
+
+        sharedPreferences = getApplicationContext().getSharedPreferences("mypref", 0);
+        editor = sharedPreferences.edit();
+        apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+        progressDialog = new ProgressDialog(this);
+    }
+
+    void UploadDataToServer()
+    {
+        progressDialog.setMessage("UPLOADING ...PLEASE WAIT");
+        progressDialog.show();
+        final String Token = sharedPreferences.getString("token","xxx");
+
+        //patch journey point at server
+        Map<String, Object> JpParams = new HashMap<>();
+        JpParams.put("journey_point","pod15_complete");
+        JpParams.put("UnixTimeOfLastSync",""+System.currentTimeMillis());
+        JpParams.put("IsAlertFromPtToServ",false);
+        JpParams.put("AlertMsgFromPtToServ","");
+
+
+
+
+
+        Call<JourneyData> call2 = apiInterface.patchData("TOKEN " + Token,JpParams );
+        call2.enqueue(new Callback<JourneyData>() {
+            @Override
+            public void onResponse(Call<JourneyData> call, Response<JourneyData> response) {
+                if(response.isSuccessful())
+                {
+                    progressDialog.dismiss();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<JourneyData> call, Throwable t) {
+
+            }
+        });
+    }
+
+    public void clik(View view) {
     }
 }
